62d01cff6249f8162a2f7b861265027143e2b55d
HeWeMel <69761336+HeWeMel@users.noreply.github.com>
2020-12-17 22:27:10 +0100

    commit

--- 2020/17/Teil2 V3.py
+++ 2020/17/Teil2 V3.py
@@ -1,79 +1,72 @@
 import sys
-import re
 import itertools
-import functools
-import collections
 
 # read input
 with open('input.txt') as f:
     lines = f.readlines()  # # read complete file, results in list of lines with endings
 
-state = collections.defaultdict( int )
+
+state = set()
 sz = len(lines[0].rstrip('\n'))
-w=z=0
+w = z = 0
 for y in range(0, sz):
-    #print (y)
     s = lines[y]
     s = s.rstrip('\n')
     for x in range(0, sz):
-        i = 0 if s[x] == "." else 1
-        state [ (w, x, y, z) ] = i
+        if s[x] == "#":
+            state.add( (w, x, y, z) )
 dimensions = 4
 
-
-# print state
+'''
+# print state (used only for debugging)
 def printstate(ranges_from, ranges_to):
-    for w in range(ranges_from[0], ranges_to[0]+1):
-        for z in range (ranges_from[3], ranges_to[3]+1):
-            print ("Content for w", w, "z", z, ":")
-            for y in range (ranges_from[2], ranges_to[2]+1):
+    print ( ranges_from, ranges_to)
+
+    for w in range(ranges_from[0], ranges_to[0] + 1):
+        for z in range(ranges_from[3], ranges_to[3] + 1):
+            print("Content for w", w, "z", z, ":")
+            for y in range(ranges_from[2], ranges_to[2] + 1):
                 s = ""
-                for x in range (ranges_from[1], ranges_to[1]+1):
-                    s = s + ( "." if state[(w, x, y, z)] == 0 else "#" )
-                print (s)
+                for x in range(ranges_from[1], ranges_to[1] + 1):
+                    s = s + ("." if (w, x, y, z) in state else "#")
+                print(s)
     print("--------------")
+'''
 
+def sizes():
+    # for state keys with state content 1: length, vector of min and of max coordinates
+    occupiedStates = list(state)
+    return len(occupiedStates), list(map(min, zip(*occupiedStates))), list(map(max, zip(*occupiedStates)))
 
-# utility functions
-def elementwise_min(v, w):              # elementwise min of two vectors
-    return itertools.starmap(min, zip(v, w))
-def elementwise_max(v, w):              # elementwise max of two vectors
-    return itertools.starmap(max, zip(v, w))
-def elementwise_plus(v, w):             # elementwise sum of two vectors
-    return itertools.starmap(sum, zip(v, w))
-
-def sizes():                            # list of min and list of max coordinates in state keys with state content 1
-    return len( [key for key in state.keys() if state[key] == 1] ),\
-        [ coords for coords in functools.reduce( elementwise_min, [key for key in state.keys() if state[key] == 1] ) ],\
-        [ coords for coords in functools.reduce( elementwise_max, [key for key in state.keys() if state[key] == 1] ) ]
 
 # process data
-zero_vector = dimensions*(0,)           # vector of zeros of needed dimension
-diffs = [diffvector for diffvector in   # all vectors "around" zero vector, without the zero vector itself
-         itertools.product(range(-1, 2), repeat=dimensions) if diffvector != zero_vector]
+zero_vector = dimensions * (0,)  # vector of zeros of needed dimension
+diffs = [diffvector for diffvector in  # all vectors "around" zero vector, without the zero vector itself
+         itertools.product(range(-1, 2), repeat=dimensions)
+         if diffvector != zero_vector]
 
-na, mins, maxs = sizes()                # number of elements in state with content 1, vectors of min and max of used range in state
+na, mins, maxs = sizes()  # for state elements with content 1: count, vectors of min and of max coordinates
 
-for cycle in range(0,6): # 6
-    modl = []
+for cycle in range(0, 6):  # 6
+    addl = set()
+    removel = set()
     # iterate the state matrix, with 1 less at the range mins and 1 more at the range max
-    ranges = [ [p for p in range(min_val-1, max_val+2)] for (min_val, max_val) in  zip(mins, maxs) ]
-    for pos in itertools.product( *ranges ):
-        a = state[pos]
-        neighbors = [ tuple([np for np in map(sum, zip(pos, d))]) for d in diffs ]  # pos+d for all d in diffs
-        no = len( [np for np in neighbors if state[np] == 1] )      # number of neighbor fields with content 1
+    ranges = [list(range(min_val - 1, max_val + 2)) for (min_val, max_val) in zip(mins, maxs)]
+    for pos in itertools.product(*ranges):
+        a = (pos in state)
+        neighbors = [tuple(map(sum, zip(pos, d))) for d in diffs]  # pos+d for all d in diffs
+        no = len([np for np in neighbors if np in state])  # number of neighbor fields with content 1
         # apply rules for changing state, generate modification list
-        if a == 0 and no == 3:
-            modl.append( (pos, 1) )
-        if a == 1 and not (no ==2 or no ==3):
-            modl.append( (pos, 0) )
-
+        if not a and no == 3:
+            addl.add(pos)
+        if a and not (no == 2 or no == 3):
+            removel.add(pos)
     # apply calculated changes in state
-    for (pos, m) in modl:
-        state[pos] = m
-
+    state = state.union(addl)
+    state = state.difference(removel)
     # recalculate size of used part of state
     na, mins, maxs = sizes()
 
-print ("res", na)
+
+print("res", na)
 # 2296
