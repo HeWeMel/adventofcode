98bbdb45f3d6e06738f0691381f2d36b37455b6f
HeWeMel <69761336+HeWeMel@users.noreply.github.com>
2020-12-20 14:43:55 +0100

    commit

---
+++ 2020/18/Teil2 V3 with pyParse.py
@@ -0,0 +1,108 @@
+# Parser für die Grundrechenarten über Integer
+# In line 31+32 and 44+45, addop and multop exchanged, since AOC task required exchanged priority
+
+from pyparsing import (
+    Literal,
+    Word,
+    nums,
+    Group,
+    Forward,
+    ParseException,
+    Suppress
+)
+import operator
+
+exprStack = []
+
+
+def push_first(toks):
+    exprStack.append(toks[0])
+
+
+bnf = None
+
+
+def BNF():
+    """
+    multop  :: '*' | '/'
+    addop   :: '+' | '-'
+    integer :: ['+' | '-'] '0'..'9'+
+    atom    :: integer | '(' expr ')'
+    term    :: atom [ addop atom ]*
+    expr    :: term [ multop term ]*
+    """
+    global bnf
+    if not bnf:
+        intNumber = Word(nums)
+        plus, minus, mult, div = map(Literal, "+-*/")
+        lpar, rpar = map(Suppress, "()")
+        addop = plus | minus
+        multop = mult | div
+
+        expr = Forward()
+        atom = intNumber.setParseAction(push_first) | Group(lpar + expr + rpar)
+        term = atom + (addop + atom).setParseAction(push_first)[...]
+        expr <<= term + (multop + term).setParseAction(push_first)[...]  # <<= sets following exp as body of last forward
+        bnf = expr
+    return bnf
+
+
+opn = {
+    "+": operator.add,
+    "-": operator.sub,
+    "*": operator.mul,
+    "/": operator.floordiv
+}
+
+
+def evaluate_stack(s):
+    op, num_args = s.pop(), 0
+    if op in "+-*/^":
+        # note: operands are pushed onto the stack in reverse order
+        op2 = evaluate_stack(s)
+        op1 = evaluate_stack(s)
+        return opn[op](op1, op2)
+    else:
+        return int(op)
+
+
+def eval(s):
+    exprStack[:] = []
+    try:
+        results = BNF().parseString(s, parseAll=True)
+        val = evaluate_stack(exprStack[:])
+    except ParseException as pe:
+        print(s, "failed parse:", str(pe))
+    except Exception as e:
+        print(s, "failed eval:", str(e), exprStack)
+    return val
+
+
+# -------------------------------------------------------------------------------
+
+with open('input.txt') as f:
+    lines = f.readlines()  # # read complete file, results in list of lines with endings
+
+# For using test input instead of file input rename from lines_example to lines
+linesex='''
+1 + 2 * 3 + 4 * 5 + 6
+1 + (2 * 3) + (4 * (5 + 6))
+2 * 3 + (4 * 5)
+5 + (8 * 3 + 9 + 3 * 4 * 3)
+5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))
+((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2
+'''[1:-1].split('\n')   # split example in lines, keep line endings
+
+
+total = 0
+for line in lines:
+    s = line.rstrip('\n')
+    s = s.replace(' ', '')
+    print ("string:", s)
+    i = eval(s)
+    print ("ergebnis:", s, i)
+    print("----------------------------")
+    total += i
+
+print ("total", total)
+# 340789638435483
