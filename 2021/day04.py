0728a5b09501e3811778eb8d5e74317ecbead41b
HeWeMel <69761336+HeWeMel@users.noreply.github.com>
2021-12-04 17:05:42 +0100

    2021/4 improved

--- 2021/day04.py
+++ 2021/day04.py
@@ -5,28 +5,16 @@ import re
 class PartA(Day):
     def parse(self, text, d):  # store puzzle parsing result data into attributes of d
         blocks_of_ints = [[int(i) for i in re.findall(r"[0-9]+", block.replace("\n", " "))]
-                       for block in text.split("\n\n")]
+                          for block in text.split("\n\n")]
 
         d.draws = blocks_of_ints[0]
 
-        d.numbers = []  # from block number to numbers in block
-        d.rows_and_columns = []  # from block number to row and number sets withs their numbers
-
+        d.value_pos_in_block = []  # from block number to dict from value to pos (x, y)
         for block in blocks_of_ints[1:]:
-            i_gen = (i for i in block)
-
-            columns = [set() for i in range(5)]
-            rows = [set() for i in range(5)]
-            fields = set()
-            for y in range(5):
-                for x in range(5):
-                    i = next(i_gen)
-                    fields.add(i)
-                    columns[x].add(i)
-                    rows[y].add(i)
-            d.numbers.append(fields)
-            d.rows_and_columns.append(columns + rows)
-        d.player_count = len(d.numbers)
+            i_gen = iter(block)
+            pos_of_values = {next(i_gen): (x, y) for x in range(5) for y in range(5)}
+            d.value_pos_in_block.append(pos_of_values)
+        d.player_count = len(d.value_pos_in_block)
 
     def play(self, d, first_wins):
         winners = set()
@@ -34,13 +22,15 @@ class PartA(Day):
             for block in range(d.player_count):
                 if block in winners:
                     continue
-                d.numbers[block].discard(draw)
-                for group in d.rows_and_columns[block]:
-                    group.discard(draw)
-                    if len(group) == 0:
+                pos_of_values = d.value_pos_in_block[block]
+                pos = pos_of_values.pop(draw, None)
+                if pos is not None:
+                    col_size = sum(1 for i, (x, y) in pos_of_values.items() if x == pos[0])
+                    row_size = sum(1 for i, (x, y) in pos_of_values.items() if y == pos[1])
+                    if col_size == 0 or row_size == 0:
                         winners.add(block)
                         if first_wins or len(winners) == d.player_count:
-                            return sum(i for i in d.numbers[block]) * draw
+                            return sum(i for i, pos in pos_of_values.items()) * draw
         return None
 
     def compute(self, d):  # return puzzle result, get parsing data from attributes of d
