6c12e60cc2ee5b4338d758001a3b8ee07ac68dc1
HeWeMel <69761336+HeWeMel@users.noreply.github.com>
2021-12-07 08:38:37 +0100

    2021/7 changed to median solution for part A

--- 2021/day07.py
+++ 2021/day07.py
@@ -5,23 +5,25 @@ class PartA(Day):
     def parse(self, text, d):  # store puzzle parsing result data into attributes of d
         d.list = [int(n) for n in text.split(",")]  # 0,1,2,3
 
-    def fuel(self, dist):
-        return dist
-
     def compute(self, d):  # return puzzle result, get parsing data from attributes of d
-        min_c = min(d.list)
-        max_c = max(d.list)
-        f_min = self.fuel(max_c - min_c) * len(d.list)
-        for p in range(min_c, max_c + 1):
-            f = sum(self.fuel(abs(s - p)) for s in d.list)
-            if f < f_min:
-                f_min = f
-        return f_min
+        d.list.sort()
+        p = d.list[len(d.list) // 2]
+        f = sum(abs(s - p) for s in d.list)
+        return f
 
 
 class PartB(PartA):
-    def fuel(self, dist):
-        return dist * (dist+1) // 2
+    def compute(self, d):  # return puzzle result, get parsing data from attributes of d
+        def fuel(dist): return dist * (dist + 1) // 2
+
+        min_c = min(d.list)
+        max_c = max(d.list)
+        f_min = fuel(max_c - min_c) * len(d.list)
+        for p in range(min_c, max_c + 1):
+            f = sum(fuel(abs(s - p)) for s in d.list)
+            if f < f_min:
+                f_min = f
+        return f_min
 
 
 Day.do_day(day=7, year=2021, part_a=PartA, part_b=PartB)
