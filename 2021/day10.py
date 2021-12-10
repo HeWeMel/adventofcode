b154bcebff7904bf924332fb5d59b4c4828892e5
HeWeMel <69761336+HeWeMel@users.noreply.github.com>
2021-12-10 08:51:21 +0100

    2021/10 improved

--- 2021/day10.py
+++ 2021/day10.py
@@ -1,46 +1,36 @@
 from mylib.aoc_frame import Day
+import functools
 
 
 class PartA(Day):
     def parse(self, text, d):  # store puzzle parsing result data into attributes of d
-        lines = text.splitlines()
-        d.closing = {'(': ')', '[': ']', '{': '}', '<': '>'}
-        scores = {')': 3, ']': 57, '}': 1197, '>': 25137}
-
-        d.r1 = 0
-        d.incomplete = []
-        for line in lines:
-            s = []
-            ok = True
+        closing = {'(': ')', '[': ']', '{': '}', '<': '>'}
+        d.first_wrong = []
+        d.not_closed = []
+        for line in text.splitlines():
+            needed_closing_brackets = []
             for c in line:
-                if c in ('(', '[', '{', '<'):
-                    s.append(c)
+                if c in closing.keys():
+                    needed_closing_brackets.append(closing[c])
                 else:
-                    co = s.pop()
-                    if c != d.closing[co]:
-                        d.r1 += scores[c]
-                        ok = False
-                        continue
-            if ok:
-                d.incomplete.append(s)
+                    co = needed_closing_brackets.pop()
+                    if c != co:
+                        d.first_wrong.append(c)
+                        break
+            else:
+                d.not_closed.append(needed_closing_brackets)
 
     def compute(self, d):  # return puzzle result, get parsing data from attributes of d
-        return d.r1
+        scores = {')': 3, ']': 57, '}': 1197, '>': 25137}
+        return sum(scores[c] for c in d.first_wrong)
 
 
 class PartB(PartA):
     def compute(self, d):  # return puzzle result, get parsing data from attributes of d
         scores2 = {')': 1, ']': 2, '}': 3, '>': 4}
-        r2 = []
-        for i in d.incomplete:
-            r2t = 0
-            for c in reversed(i):
-                r2t = r2t * 5 + scores2[d.closing[c]]
-            r2.append(r2t)
-
-        r2 = sorted(r2)
-        r3 = r2[(len(r2)-1)//2]
-        return r3
+        score = [functools.reduce(lambda a, b: a*5+b, (scores2[c] for c in reversed(i)))
+                 for i in d.not_closed]
+        return sorted(score)[len(score)//2]
 
 
 Day.do_day(day=10, year=2021, part_a=PartA, part_b=PartB)
