8621bcb0b0f02689271dc40873f0a1e18988edda
HeWeMel <69761336+HeWeMel@users.noreply.github.com>
2021-12-21 07:59:04 +0100

    2021/21 solved

---
+++ 2021/day21.py
@@ -0,0 +1,59 @@
+import functools
+from mylib.aoc_frame import Day
+
+
+class PartA(Day):
+    def parse(self, text, d):  # store puzzle parsing result data into attributes of d
+        text = text.splitlines()
+        d.start_positions = int(text[0][-1]), int(text[1][-1])
+
+    def compute(self, d):  # return puzzle result, get parsing data from attributes of d
+        position_player1, position_player2 = d.start_positions[0], d.start_positions[1]
+        dice_rolls = score_player1 = score_player2 = 0
+
+        def roll_dice():
+            while True:
+                for v in range(1, 101):
+                    yield v
+        dice = iter(roll_dice())
+
+        while True:
+            position_player1 += next(dice) + next(dice) + next(dice)
+            while position_player1 > 10:
+                position_player1 -= 10
+            dice_rolls += 3
+            score_player1 += position_player1
+            if score_player1 >= 1000:
+                return dice_rolls * score_player2
+            position_player1, position_player2, score_player1, score_player2 = \
+                position_player2, position_player1, score_player2, score_player1
+
+
+class PartB(PartA):
+    def compute(self, d):  # return puzzle result, get parsing data from attributes of d
+        wins = do(0, d.start_positions, (0, 0))
+        return max(wins)
+
+
+@functools.cache
+def do(player, positions, scores):
+    wins = [0, 0]
+    for d1 in range(1, 4):
+        for d2 in range(1, 4):
+            for d3 in range(1, 4):
+                new_positions = list(positions)
+                new_scores = list(scores)
+                new_positions[player] += d1 + d2 + d3
+                while new_positions[player] > 10:
+                    new_positions[player] -= 10
+                new_scores[player] += new_positions[player]
+                if new_scores[player] >= 21:
+                    wins[player] += 1
+                else:
+                    found_wins = do(1-player, tuple(new_positions), tuple(new_scores))
+                    wins[0] += found_wins[0]
+                    wins[1] += found_wins[1]
+    return wins
+
+
+Day.do_day(day=21, year=2021, part_a=PartA, part_b=PartB)
