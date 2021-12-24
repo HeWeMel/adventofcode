3eaf30c6cc1b76b26f9349e81f8ffee9c1162f3f
HeWeMel <69761336+HeWeMel@users.noreply.github.com>
2021-12-24 13:57:06 +0100

    2021/24 made a general solution

--- 2021/day24.py
+++ 2021/day24.py
@@ -2,16 +2,17 @@ import functools
 import math
 from mylib.aoc_frame import Day
 
+values = []
+
 
 def run(z, w, i):
-    values1 = (1, 1, 1, 1, 26, 26, 26, 1, 1, 26, 26, 26, 1, 26)
-    values2 = (13, 15, 15, 11, -16, -11, -6, 11, 10, -10, -8, -11, 12, -15)
-    values3 = (5, 14, 15, 16, 8, 9, 2, 13, 16, 6, 6, 9, 11, 5)
-
+    """ simulate a run of part i of the 14 parts of the program, giving a starting value
+    for variable z and the input digit as w """
+    v1, v2, v3 = values[i]
     x = z
     x = x - math.trunc(x / 26) * 26
-    z = math.trunc(z / values1[i])  # z // values1[i]
-    x += values2[i]
+    z = math.trunc(z / v1)
+    x += v2
     x = 1 if x == w else 0
     x = 1 if x == 0 else 0
     y = 25
@@ -19,7 +20,7 @@ def run(z, w, i):
     y += 1
     z *= y
     y = w
-    y += values3[i]
+    y += v3
     y *= x
     z += y
     return z
@@ -27,31 +28,43 @@ def run(z, w, i):
 
 @functools.cache
 def best(z, i, down):
+    """ find best number (first possible downwards or upwards) that leads to z == zero at end"""
     for digit in (range(9, 0, -1) if down else range(1, 10, 1)):
         z_after = run(z, digit, i)
-        if i == 13:
+        if i == 13:  # at the end of the number, z must be zero
             if z_after == 0:
                 return digit
-        else:
-            res = best(z_after, i + 1, down)
+        else:  # for other digit positions in the number, try to continue search recursively
+            res = best(z_after, i + 1, down)  # z value resulting from my digit is start z for rest
             if res is None:
                 continue
-            return digit * 10 ** (13 - i) + res
+            return digit * 10 ** (13 - i) + res  # if recursion finds solution, add my digit
     return None
 
 
 class PartA(Day):
-    def compute(self, d):  # return puzzle result, get parsing data from attributes of d
-        res = best(0, 0, down=True)
+    def parse(self, text, d):
+        """ get variable values out of program, for each of the 14 parts """
+        global values
+        prg = text.splitlines()
+        part_len = len(prg) // 14
+        for part in range(14):
+            v1 = int(prg[4].split(" ")[2])
+            v2 = int(prg[5].split(" ")[2])
+            v3 = int(prg[15].split(" ")[2])
+            values.append((v1, v2, v3))
+            prg = prg[part_len:]
         best.cache_clear()
-        return res
+
+    def compute(self, d):  # return puzzle result, get parsing data from attributes of d
+        """ to find highest possible value, search for each digit downwards """
+        return best(0, 0, down=True)
 
 
 class PartB(PartA):
     def compute(self, d):  # return puzzle result, get parsing data from attributes of d
-        res = best(0, 0, down=False)
-        best.cache_clear()
-        return res
+        """ to find lowest possible value, search for each digit upwards """
+        return best(0, 0, down=False)
 
 
 Day.do_day(day=24, year=2021, part_a=PartA, part_b=PartB)
