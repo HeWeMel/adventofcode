57f10a1b1c86f20e2ce34e56bb783dea7f0ce092
HeWeMel <69761336+HeWeMel@users.noreply.github.com>
2022-12-30 11:56:08 +0100

    BFS of outer area instead of inner area (closer to the problem description, thus safer)

--- 2022/day18.py
+++ 2022/day18.py
@@ -6,7 +6,7 @@ class PartA(Day):
     def parse(self, text, d):  # store puzzle parsing result data into attributes of d
         d.cubes = set(nog.Position(int(w) for w in line.split(","))
                       for line in text.splitlines())
-        d.moves = ((-1, 0, 0), (0, -1, 0), (0, 0, -1), (1, 0, 0), (0, 1, 0), (0, 0, 1))
+        d.moves = nog.Position.moves(3)
 
     def compute(self, d):  # return puzzle result, get parsing data from attributes of d
         return sum(0 if neighbor in d.cubes else 1
@@ -21,32 +21,19 @@ class PartB(PartA):
     def compute(self, d):  # return puzzle result, get parsing data from attributes of d
         cubes: set[nog.Position] = d.cubes
 
-        limits = [(min(c[coord_number] for c in cubes),
-                   max(c[coord_number] for c in cubes)
+        # bounding box, not touching, upper limits are ment exclusively
+        limits = [(min(c[coord_number] - 1 for c in cubes),
+                   max(c[coord_number] + 1 + 1 for c in cubes)
                    ) for coord_number in range(3)]
 
-        def next_vertices(v_from: nog.Position, _):
-            return list(v_from.neighbors(d.moves))
+        # positions outside the cubes, reachable by orthogonal moves
+        t = nog.TraversalBreadthFirst(lambda v, _: v.neighbors(d.moves, limits))
+        low_corner = nog.Position(limit[0] for limit in limits)
+        outer = set(t.start_from(low_corner, already_visited=set(cubes)))
 
-        around_hull = set()
-        for cube in cubes:
-            around_hull.update(cube.neighbors(d.moves))
-        around_hull.difference_update(cubes)
-
-        t = nog.TraversalBreadthFirst(next_vertices)
-        inner = set()
-
-        for start in around_hull:
-            test_inner = {start}
-            for v in t.start_from(start, already_visited=set(cubes)):
-                if not v.is_in_cuboid(limits):
-                    break  # Search from start escaped cubes. Thus, it is "outside".
-                test_inner.add(v)
-            else:
-                inner.update(test_inner)
-
-        return sum(0 if neighbor in cubes or neighbor in inner else 1
-                   for cube in cubes
+        # count faces to neighbor positions that are in outer area
+        return sum(1 if neighbor in outer else 0
+                   for cube in d.cubes
                    for neighbor in cube.neighbors(d.moves))
 
     def tests(self):  # yield testcases as tuple: (test_result, correct_result [, test_name])
